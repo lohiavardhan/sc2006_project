6da945053e755a7418f7e3715a61c97dd096a4f5
shinghao <sohshinghao@gmail.com>
2022-10-13 20:14:45 +0800

    Main Style.css

--- dev/client/src/components/App.js
+++ dev/client/src/components/App.js
@@ -8,48 +8,37 @@ import Account from "./Account";
 import EditAccount from "./EditAccount";
 import Wishlist from "./Wishlist";
 import Navbar from "./Navbar";
+import "../../static/css/styles.css";
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-    Redirect,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect,
 } from "react-router-dom";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <>
-                <Navbar />
-                <Router>
-                    <Routes>
-                        <Route path="/signup" element={<Signup />} />
-                        <Route
-                            path="signup/authenticate/:email"
-                            element={<EmailAuth />}
-                        />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route
-                            path="accounts/:username"
-                            element={<Account />}
-                        />
-                        <Route
-                            path="accounts/:username/edit"
-                            element={<EditAccount />}
-                        />
-                        <Route
-                            path="accounts/:username/wishlist"
-                            element={<Wishlist />}
-                        />
-                    </Routes>
-                </Router>
-            </>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Router>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="signup/authenticate/:email" element={<EmailAuth />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="accounts/:username" element={<Account />} />
+            <Route path="accounts/:username/edit" element={<EditAccount />} />
+            <Route path="accounts/:username/wishlist" element={<Wishlist />} />
+          </Routes>
+        </Router>
+      </>
+    );
+  }
 }
 
 const AppDiv = document.getElementById("app");
