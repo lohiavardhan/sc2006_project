4353465a525cc627a67f6ded3632fa6d56f58378
lohiavardhan <lohiavardhan@gmail.com>
2022-10-17 17:18:38 +0800

    deleting node_modules

--- dev/client/src/components/App.js
+++ dev/client/src/components/App.js
@@ -25,6 +25,7 @@ export default class App extends Component {
     render() {
         return (
             <>
+                <Navbar/>
                 <Router>
                     <Routes>
                         <Route path="/signup" element={<Signup />} />
