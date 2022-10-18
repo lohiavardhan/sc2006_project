8b0da33b43d9144eff0dfd030a6fb58a23939d11
shinghao <sohshinghao@gmail.com>
2022-10-19 00:45:48 +0800

    Side Bar

--- dev/client/src/components/AccountSideBar.js
+++ dev/client/src/components/AccountSideBar.js
@@ -3,39 +3,54 @@ import React, { Component } from "react";
 import { useParams } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
-export default class AccountSideBar extends Component {
+class AccountSideBar extends Component {
   constructor(props) {
     super(props);
-    // let { username } = this.props.params;
-    // this.state = {
-    //   username: username,
-    // };
+    this.state = {
+      username: "",
+      isAuth: true,
+      redirect: false,
+    };
   }
-
-  //   componentDidMount() {
-  //     fetch("/api/v1/accounts?username=" + this.state.username)
-  //       .then((response) => {
-  //         return response.json();
-  //       })
-  //       .then((json) => {
-  //         if (json.error == "status_OK") {
-  //           this.setState({
-  //             username: json.username,
-  //           });
-  //         } else {
-  //           this.setState({
-  //             isAuth: false,
-  //           });
-  //         }
-  //       });
-  //   }
 
   render() {
-    return (
-      <>
-        <div className="sidebar-container"></div>
-        {/* <a href={`/accounts/${username}/friends/view`}> View Friends </a> */}
-      </>
-    );
+    const { username } = this.state;
+    const { redirect } = this.state;
+    const { isAuth } = this.state;
+
+    {
+      return (
+        <>
+          <div className="sidebar-container">
+            <div className="circle"></div>
+            <a
+              className="sidebar-container__tab sidebar-container__tab--top"
+              href={`/accounts/${username}`}
+            >
+              <p>Account Overview</p>
+            </a>
+            <a
+              className="sidebar-container__tab"
+              href={`/accounts/${username}/friends/view`}
+            >
+              <p>Friends</p>
+            </a>
+
+            <a
+              className="sidebar-container__tab"
+              href={`/accounts/${username}/friends/view`}
+            >
+              <p>Friend Requests</p>
+            </a>
+          </div>
+        </>
+      );
+    }
   }
 }
+
+function withParams(Component) {
+  return (props) => <Component {...props} params={useParams()} />;
+}
+
+export default withParams(AccountSideBar);
