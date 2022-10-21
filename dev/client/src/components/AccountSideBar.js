a7e56e7e460fff288b4f97a1546a9121183c157a
shinghao <sohshinghao@gmail.com>
2022-10-22 04:30:38 +0800

    Removed 'Friend Request' btn from account side bar

--- dev/client/src/components/AccountSideBar.js
+++ dev/client/src/components/AccountSideBar.js
@@ -4,69 +4,69 @@ import { useParams } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
 class AccountSideBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            isAuth: true,
-            redirect: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      isAuth: true,
+      redirect: false,
+    };
+  }
 
-    componentDidMount() {
-        fetch("/api/v1/accounts/login")
-            .then((response) => {
-                return response.json();
-            })
-            .then((json) => {
-                if (json.error == "status_invalid_access") {
-                    this.setState({
-                        username: json.username,
-                    });
-                }
-            });
-    }
+  componentDidMount() {
+    fetch("/api/v1/accounts/login")
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        if (json.error == "status_invalid_access") {
+          this.setState({
+            username: json.username,
+          });
+        }
+      });
+  }
 
-    render() {
-        const { username } = this.state;
-        const { redirect } = this.state;
-        const { isAuth } = this.state;
+  render() {
+    const { username } = this.state;
+    const { redirect } = this.state;
+    const { isAuth } = this.state;
 
-        {
-            return (
-                <>
-                    <div className="sidebar-container">
-                        <div className="circle">
-                            <i className="fa-solid fa-user"></i>
-                        </div>
-                        <a
-                            className="sidebar-container__tab sidebar-container__tab--top"
-                            href={`/accounts/${username}`}
-                        >
-                            <p>Account Overview</p>
-                        </a>
-                        <a
-                            className="sidebar-container__tab"
-                            href={`/accounts/${username}/friends/view`}
-                        >
-                            <p>Friends</p>
-                        </a>
+    {
+      return (
+        <>
+          <div className="sidebar-container">
+            <div className="circle">
+              <i className="fa-solid fa-user"></i>
+            </div>
+            <a
+              className="sidebar-container__tab sidebar-container__tab--top"
+              href={`/accounts/${username}`}
+            >
+              <p>Account Overview</p>
+            </a>
+            <a
+              className="sidebar-container__tab"
+              href={`/accounts/${username}/friends/view`}
+            >
+              <p>Friends</p>
+            </a>
 
-                        <a
+            {/* <a
                             className="sidebar-container__tab"
                             href={`/accounts/${username}/friends/view`}
                         >
                             <p>Friend Requests</p>
-                        </a>
-                    </div>
-                </>
-            );
-        }
+                        </a> */}
+          </div>
+        </>
+      );
     }
+  }
 }
 
 function withParams(Component) {
-    return (props) => <Component {...props} params={useParams()} />;
+  return (props) => <Component {...props} params={useParams()} />;
 }
 
 export default withParams(AccountSideBar);
