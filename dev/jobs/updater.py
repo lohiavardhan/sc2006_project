5d51a81ec4ed7a7c65d3b4c349109f18ba471fc6
Neo-Zenith <leejuin.office@gmail.com>
2022-10-25 01:56:04 +0800

    Update to SRS

--- dev/jobs/updater.py
+++ dev/jobs/updater.py
@@ -3,6 +3,6 @@ from .jobs import checkBirthdayNotifications, keepServerAwake
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(checkBirthdayNotifications, 'interval', hours=24)
+    scheduler.add_job(checkBirthdayNotifications, 'interval', hours=4)
     scheduler.add_job(keepServerAwake, 'interval', seconds=1000)
     scheduler.start()
